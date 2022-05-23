b8fdd46f8bd559a510adddd5c15ddd47e03d2b2c
chris <christophedeslandes@hotmail.fr>
2022-05-23 12:20:20 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'exercice';
 
-  ngOnInit(): void {
-      
-  }
+  ngOnInit(): void {   
+    
+}
 }
 
